b55ce9eeb68caa4e4032d93ba9a4c6576fc64513
יוֹנָתָן <jonDel@users.noreply.github.com>
2016-12-08 08:53:34 -0200

    Fixing relative path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,7 +28,7 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
-sys.path.append(os.path.join(os.path.dirname(__name__), '../serenata-toolbox'))
+sys.path.append(os.path.join(os.path.dirname(__name__), '../serenata_toolbox'))
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
