1dea766c5b0c20761bf07fa33f6c1c6ae1540b2d
יוֹנָתָן <jonDel@users.noreply.github.com>
2016-12-08 09:44:25 -0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,9 +29,8 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 sys.path.append(os.path.join(os.path.dirname(__name__), '../serenata_toolbox'))
-#sys.path.append(os.path.join(os.path.dirname(__name__), '../../serenata_toolbox'))
 sys.path.append(os.path.abspath('../../serenata_toolbox'))
-
+print 'test py3 execution'
 
 
 # Add any Sphinx extension module names here, as strings. They can be
